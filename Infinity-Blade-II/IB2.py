bcdacab6329357796d2681d91990f00bf2f39833
avinassh <avinashsajjan@gmail.com>
2013-12-23 10:38:34 +0530

    updated code and changed the url

--- Infinity-Blade-II/IB2.py
+++ Infinity-Blade-II/IB2.py
@@ -3,7 +3,8 @@ import urllib2
 from time import sleep
 
 def returnstr():
-    return urllib2.urlopen('http://anonymouse.org/cgi-bin/anon-www.cgi/http://www.ign.com/private/prime/promo/infinity-blade-2-free/code').read()
+    #return urllib2.urlopen('http://anonymouse.org/cgi-bin/anon-www.cgi/http://www.ign.com/private/prime/promo/infinity-blade-2-free/code').read()
+    return urllib2.urlopen('http://anonymouse.org/cgi-bin/anon-www.cgi/http://new-url-here.com/')
 
 for i in range(1, 500):
     string = returnstr()[returnstr().find('"code":')+8:returnstr().find('"res')]
